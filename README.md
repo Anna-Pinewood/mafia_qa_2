b2e7fbbf0c133e593a5aa53daa138e0de0aa79f9
Olga Lipina <neon_alfa@mail.ru>
2024-12-16 14:14:24 +0000

    Init db script.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 
-Get chroma password by running `docker run --rm --entrypoint htpasswd httpd:2 -Bbn admin YOUR_PASSWORD > server.htpasswd`
+1. fill env
+2. Get chroma password by running `docker run --rm --entrypoint htpasswd httpd:2 -Bbn admin YOUR_PASSWORD > server.htpasswd`
+3. `python src/database/init_rag.py`  for initting tables.
---
+++ src/consts.py
@@ -0,0 +1,22 @@
+import os
+from dotenv import dotenv_values
+from pathlib import Path
+path_project = Path(__file__).resolve().parents[1]
+env_values = dotenv_values(path_project/'.env')
+print(env_values)
+# Chroma DB related constants
+CHROMA_HOST = os.environ.get(
+    'CHROMA_HOST', default=env_values['CHROMA_HOST'])
+CHROMA_PORT = int(
+    os.environ.get('CHROMA_PORT', default=env_values['CHROMA_PORT'])
+)
+CHROMA_SERVER_AUTHN_PROVIDER = os.environ.get(
+    'CHROMA_SERVER_AUTHN_PROVIDER', default=env_values['CHROMA_SERVER_AUTHN_PROVIDER'])
+CHROMA_CLIENT_AUTHN_PROVIDER = os.environ.get(
+    'CHROMA_CLIENT_AUTHN_PROVIDER', default=env_values['CHROMA_CLIENT_AUTHN_PROVIDER'])
+CHROMA_PASSWORD = os.environ.get(
+    'CHROMA_PASSWORD', default=env_values['CHROMA_PASSWORD'])
+CHROMA_SERVER_AUTHN_CREDENTIALS_FILE = os.environ.get(
+    'CHROMA_SERVER_AUTHN_CREDENTIALS_FILE', default=env_values['CHROMA_SERVER_AUTHN_CREDENTIALS_FILE'])
+CHROMA_USER = os.environ.get(
+    'CHROMA_USER', default=env_values['CHROMA_USER'])
